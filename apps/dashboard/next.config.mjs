7adc2bbaff41dd8b0749fe8e43f18e7085dca73a
P. Mark Anderson <mark@martianrover.com>
2026-08-21 01:18:17 -0700

    config/next: allow remote hosts to tailscale vpn can work from phone

--- apps/dashboard/next.config.mjs
+++ apps/dashboard/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  allowedDevOrigins: ["arcadia-1.alpine-rattlesnake.ts.net"],
   webpack(config) {
     config.resolve.extensionAlias = {
       ...(config.resolve.extensionAlias ?? {}),
